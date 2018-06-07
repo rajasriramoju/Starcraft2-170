6a0e3373c352325fd223d3b42e980beef4498c20
Raja Vyshnavi Sriramoju <rsriramoju@ucdavis.edu>
2018-06-06 21:40:27 -0700

    Changed the column headers, load array into data frame

--- load_data_script.py
+++ load_data_script.py
@@ -13,14 +13,15 @@ from tabulate import tabulate
 #this is where the files will be read into arrays then the arrays will be put into an array
 for input_file in glob.glob("*.npy"):
 	data = np.load(input_file)
-	column_names = np.array(['Cumulative','Frame ID','Minerals','Vespene','Food Cap','Food Used','Food Army','Food Worker','Idle Worker Count','Army Count','Warp Gate Count','Probe','Zealot','Stalker','Sentry','Adept','High Templar','Dark Templar','Immortal','Colossus','Disruptor','Archon','Observer','Warp Prism','Phoenix','Void Ray','Oracle','Carrier','Tempest','Mothership','Enemy Probe','Enemy Zealot','Enemy Stalker','Enemy Sentry','Enemy Adept','Enemy High Templar','Enemy Dark Templar','Enemy Immortal','Enemy Colossus','Enemy Disruptor','Enemy Archon','Enemy Observer','Enemy Warp Prism','Enemy Phoenix','Enemy Void Ray','Enemy Oracle','Enemy Carrier','Enemy Tempest','Enemy Mothership','Most Beneficial Unit'])
-	np.concatenate([column_names,data])
+	column_names = ['Cumulative','Frame ID','Minerals','Vespene','Food Cap','Food Used','Food Army','Food Worker','Idle Worker Count','Army Count','Warp Gate Count','Probe','Zealot','Stalker','Sentry','Adept','High Templar','Dark Templar','Immortal','Colossus','Disruptor','Archon','Observer','Warp Prism','Phoenix','Void Ray','Oracle','Carrier','Tempest','Mothership','Enemy Probe','Enemy Zealot','Enemy Stalker','Enemy Sentry','Enemy Adept','Enemy High Templar','Enemy Dark Templar','Enemy Immortal','Enemy Colossus','Enemy Disruptor','Enemy Archon','Enemy Observer','Enemy Warp Prism','Enemy Phoenix','Enemy Void Ray','Enemy Oracle','Enemy Carrier','Enemy Tempest','Enemy Mothership','Most Beneficial Unit']
+
+	#np.concatenate([column_names,data])
 	#process the frames and append onto frame
-	df = pd.DataFrame(data=data[1:,1:],    # values
-	    # 1st column as index
-	columns=data[0,:])  # 1st row as the column names
-	#print tabulate(df2, headers='keys', tablefmt='psql')
-	print(df2)
+	df = pd.DataFrame(data, columns = column_names)
+
+	print(df.shape)
+	df.to_csv(r'testing.txt', header=True, index=None, sep=' ', mode='a')
+
 	#neural network implementation
 	#probably 1 or 2 hiddne layers
 	#inputs depend on choices
